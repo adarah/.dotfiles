957002aaeefe33ab20945b1277ed3f89b3c0086e
Lucas Harada <lucasyharada@gmail.com>
2024-06-07 11:19:28 -0300

    only override icon on macos

--- modules/home/kitty/default.nix
+++ modules/home/kitty/default.nix
@@ -3,6 +3,7 @@
   inputs,
   lib,
   pkgs,
+  stdenv,
   ...
 }: let
   cfg = config.adarah.kitty;
@@ -12,13 +13,13 @@ in {
   };
 
   config = lib.mkIf cfg.enable {
-    programs.kitty.package = pkgs.kitty.overrideAttrs (o: {
+    programs.kitty.package = if stdenv.isDarwin then pkgs.kitty.overrideAttrs (o: {
       postInstall =
         (o.postInstall or "")
         + ''
           cp ${inputs.kitty-icon}/kitty-dark.icns $out/Applications/kitty.app/Contents/Resources/kitty.icns
         '';
-    });
+    }) else null;
     programs.kitty.enable = true;
     programs.kitty.font.name = "MesloLGS Nerd Font Mono";
     programs.kitty.font.size = 14;
